021e390d0206c9b4d387a8a76e36872739238c70
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-16 23:37:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  pets={"cat1"=>"Willow", "cat2"=>"Champ"}
 end
 
 
